1f9804a8c70db7632386f27dc6d4f3782bfb1e59
seongmin2223 <khyuno9@naver.com>
2025-08-11 08:51:49 +0900

    초기파일 생성

---
+++ src/main/java/org/example/scheduleforuser/ScheduleForUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.scheduleforuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleForUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleForUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scheduleforuser/ScheduleForUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scheduleforuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleForUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
