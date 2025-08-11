5f653783d4b26a259964fd2c546f297c5497080a
seongmin2223 <khyuno9@naver.com>
2025-08-11 09:11:42 +0900

    dto, entity, 각종 mvc생성

---
+++ src/main/java/org/example/scheduleforuser/entity/Schedule.java
@@ -0,0 +1,35 @@
+package org.example.scheduleforuser.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.scheduleforuser.dto.ScheduleRequestDto;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Schedule extends ScheduleDateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", length = 255)
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Schedule(ScheduleRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.author = requestDto.getAuthor();
+        this.password = requestDto.getPassword();
+    }
+}
